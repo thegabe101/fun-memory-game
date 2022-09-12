a41e438dd9eccdb18cd61421d874fd0a2c14751a
Rayncor <sowa332@gmail.com>
2022-09-12 03:08:08 -0700

    made card array

--- app.js
+++ app.js
@@ -1,6 +1,58 @@
 const cards = [
     {
         name: 'fries',
-        img: 
+        img: './images/fries.webp'
+    },
+    {
+        name: 'cheeseburger',
+        img: './images/Cheeseburger.jpg'
+    },
+    {
+        name: 'hot dog',
+        img: './images/hotdog.png'
+    },
+    {
+        name: 'ice cream',
+        img: './images/icecream.jpg'
+    },
+    {
+        name: 'milkshake',
+        img: './images/milkshake.jpg'
+    },
+    {
+        name: 'pizza',
+        img: './images/pizza.jpg'
+    },
+    {
+        name: 'white',
+        img: './images/white.jpg'
+    },
+    {
+        name: 'fries',
+        img: './images/fries.webp'
+    },
+    {
+        name: 'cheeseburger',
+        img: './images/Cheeseburger.jpg'
+    },
+    {
+        name: 'hot dog',
+        img: './images/hotdog.png'
+    },
+    {
+        name: 'ice cream',
+        img: './images/icecream.jpg'
+    },
+    {
+        name: 'milkshake',
+        img: './images/milkshake.jpg'
+    },
+    {
+        name: 'pizza',
+        img: './images/pizza.jpg'
+    },
+    {
+        name: 'white',
+        img: './images/white.jpg'
     },
 ]
